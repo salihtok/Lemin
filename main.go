c904db12d057353b1b29e2f0b0930e2f4e536543
stok <salih_tok1@outlook.com>
2024-06-23 14:12:23 +0300

    Working on readings

--- main.go
+++ main.go
@@ -6,10 +6,11 @@ import (
 )
 
 func main() {
-	if len(os.Args[1]) != 2 {
+	if len(os.Args) != 2 {
 		fmt.Println("Usage: go run . <mapFile>")
 		os.Exit(1)
 	}
 	readFile(os.Args[1])
+	SeperateRows() // verileri parçaladık.
 
 }
